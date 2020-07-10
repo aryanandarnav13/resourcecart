6497ba9f126387c337a470ab8118b2cc4b4e72f0
Aryanand Arnav <56192760+aryanandarnav13@users.noreply.github.com>
2020-07-11 03:11:03 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,177 @@
+const resources = {
+	python: [
+		{
+			name: "Learn Python | CodeAcademy  ",
+			link: 'https://www.codecademy.com/learn/learn-python'
+		},
+		{   name:"Progate Python Classes ",
+			link:'https://progate.com/languages/python'
+		},
+		{
+			name:"Video Tutorial for absolute beginners ",
+			link:'http://bit.ly/2NkrsKh'
+		},
+		{
+			name:"Intro to Python | Udacity",
+			link:'https://in.udacity.com/course/introduction-to-python--ud1110-india'
+
+		},
+		{
+			name:"Python For Everybody",
+			link:'https://www.coursera.org/specializations/python'
+
+		},
+		{
+			name:"Write Better Python Functions",
+			link:'https://jeffknupp.com/'
+
+		},
+		{
+			name:"Learning Python: From Zero to Hero",
+			link:'https://medium.freecodecamp.org/learning-python-from-zero-to-hero-120ea540b567'
+
+		},
+		{
+			name:"Automate the Boring Stuff with Python",
+			link:'https://automatetheboringstuff.com/'
+
+		},
+		{
+			name:"The New Boston Python | Youtube",
+			link:'https://www.youtube.com/playlist?list=PL6gx4Cwl9DGAcbMi1sH6oAMk4JHw91mC_'
+
+		},
+		{
+			name:"Think Python 2e - Green Tea Press",
+			link:'http://greenteapress.com/thinkpython2/thinkpython2.pdf'
+
+		},
+		{
+			name:"A Byte of Python",
+			link:'https://python.swaroopch.com/'
+
+		},
+		{
+			name:"Project Euler - Great for practicing writing Python codes",
+			link:'https://projecteuler.net/'
+
+		},
+		{
+			name:"A Whirlwind Tour of Python",
+			link:'https://github.com/jakevdp/WhirlwindTourOfPython'
+
+		},
+		{
+			name:"Python Data Science Handbook",
+			link:'https://github.com/jakevdp/PythonDataScienceHandbook'
+
+		},
+		{
+			name:"Python Class By Google - Recommended",
+			link:'https://developers.google.com/edu/python/'
+
+		},
+		{
+			name:"Intro to Python for Data Science",
+			link:'https://www.datacamp.com/courses/intro-to-python-for-data-science'
+
+		},
+		{
+			name:"Python 3 for humans that want practical project exposure",
+			link:'https://pythonprogramming.net/'
+
+		},
+		{
+			name:"Learn Python the Hard Way",
+			link:'https://learnpythonthehardway.org/'
+
+		},
+		{
+			name:"Best GitHub Repositories to follow",
+			link:'https://github.com/TheAlgorithms/Python'
+
+		},
+
+	],
+	ml: [
+		{
+			name:"CSE-229 - Stanford University :",
+			link:'http://cs229.stanford.edu/'
+
+		},
+		{
+			name:"AndrewNg | Coursera",
+			link:'https://www.coursera.org/learn/machine-learning'
+
+		},
+		{
+			name:"Machine Learning - Nanodegree | Udacity",
+			link:'https://in.udacity.com/course/intro-to-machine-learning--ud120-india'
+
+		},
+		{
+			name:"Reinforcement Learning - Nanodegree | Udacity",
+			link:'https://in.udacity.com/course/reinforcement-learning--ud600'
+
+		},
+		{
+			name:"Move 37 ",
+			link:'https://www.theschool.ai/courses/move-37-course/'
+
+		},
+		{
+			name:"ML with Python | YouTube",
+			link:'https://www.youtube.com/playlist?list=PLQVvvaa0QuDfKTOs3Keq_kaG2P55YRn5v'
+
+		},
+		{
+			name:"Data Science Machine Learning Bootcamp",
+			link:'https://courses.learncodeonline.in/learn/Machine-Learning-Bootcamp?tab=1'
+
+		},
+		{
+			name:"Machine Learning Crash Course | Google Developers",
+			link:'https://developers.google.com/machine-learning/crash-course/ml-intro'
+
+		},
+		{
+			name:"Applied Data Science with Python Specialization | Coursera ",
+			link:'https://www.coursera.org/specializations/data-science-python'
+
+		},
+		{
+			name:"Machine Learning | Kaggle",
+			link:'https://www.kaggle.com/learn/machine-learning'
+
+		},
+		{
+			name:"Best GitHub Repositories to follow:Self Taught Path for Data Science",
+			link:'https://github.com/ossu/data-science'
+
+		},
+		{
+			name:"Best GitHub Repositories to follow:Machine Learning Tutorials",
+			link:'https://github.com/ujjwalkarn/Machine-Learning-Tutorials'
+
+		},
+		{
+			name:"Best GitHub Repositories to follow:Coding Train -Machine Learning cheatsheets for Stanford's CS 229",
+			link:'https://github.com/afshinea/stanford-cs-229-machine-learning'
+
+		},
+		{
+			name:"Test ML Models on Datasets:Kaggle",
+			link:''
+		},
+		{
+			name:"Test ML Models on Datasets:UCI ML Repository",
+			link:''
+		},
+		{
+			name:"Test ML Models on Datasets:Data.Gov",
+			link:''
+		},
+
+	]
+}
+
